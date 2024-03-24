df1eb247bd1c6d6f87651bf6ddb1b9f1344c2d53
Oscar Anyiam <anyiam@systemspecs.com.ng>
2024-03-24 17:03:30 +0100

    feat(profile_api): Add Migration
    Add migration for models

--- profile_api/models.py
+++ profile_api/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
 
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for UserProfile"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have email address')
+        email = self.normalize_email(email)
+        user = self.model(email, name)
+
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+
+    def create_admin_user(self, email,name, password):
+        """Create admin user"""
+        user = self.create_user(email, name, password)
+        user.is_superuser= True
+        user.is_staff = True
+        user.save(using=self.db)
+
+
+class UserProfile(AbstractUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        """String Representation of a class"""
+        return self.email
